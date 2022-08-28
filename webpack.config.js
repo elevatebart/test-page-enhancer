18cec642a07ed8fa38a7a72e9865c9dc0a629379
elevatebart <bart@cypress.io>
2022-08-27 20:49:36 -0500

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const webpack = require('webpack');
+const path = require('path');
+const { VueLoaderPlugin } = require('vue-loader');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      }
+    ]
+  },
+  resolve: {
+    extensions: [
+      '.js',
+      '.vue'
+    ]
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new HtmlWebpackPlugin({
+      templateContent: ({ htmlWebpackPlugin }) => '<!DOCTYPE html><html><head><meta charset=\"utf-8\"><title>' + htmlWebpackPlugin.options.title + '</title></head><body><div id=\"app\"></div></body></html>',
+      filename: 'index.html',
+    })
+  ]
+};
+
+module.exports = config;
